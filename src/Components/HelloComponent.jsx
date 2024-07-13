e63e8d231a13c15c38f17806cb89228c4385af79
santhoshkumar24263 <santhosh24263@gmail.com>
2024-07-13 10:03:32 +0530

    first commit

---
+++ src/Components/HelloComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import App from '../App'
+import Welcome from './Welcome'
+
+const HelloComponent = () => {
+  return (
+    <div>
+       
+        <Welcome/>
+    </div>
+  )
+}
+
+export default HelloComponent
